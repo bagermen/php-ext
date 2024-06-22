03227984a391a2b80f537d77f9f1c20ed332b804
bagermen <bagermen@yandex.ru>
2024-06-22 22:38:34 +0300

    refactor: :wrench: set repository style
    add .editorconfig and convert spaces to tabs

--- tools/alpine.sh
+++ tools/alpine.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-    exit 1
+	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+	exit 1
 fi
 
 EXT_PACKAGES=${1:?}
@@ -23,301 +23,301 @@ func_list=$(echo $EXT_PACKAGES | sed -e 's/\([^[:space:]]\+\)/\1_ext/g')
 #####
 # example_ext()
 # {
-#     if [ $1 = "pkgs" ]; then
-#         echo "required packaged"
-#         return
-#     fi
+# 	if [ $1 = "pkgs" ]; then
+# 		echo "required packaged"
+# 		return
+# 	fi
 
-#     if [ $1 = "pkgs-dev" ]; then
-#         echo "packages to build"
-#         return
-#     fi
+# 	if [ $1 = "pkgs-dev" ]; then
+# 		echo "packages to build"
+# 		return
+# 	fi
 
-#     if [ $1 = "pkgs-local" ]; then
-#         echo "local-package-name1 local-package-name2"
-#         return
-#     fi
+# 	if [ $1 = "pkgs-local" ]; then
+# 		echo "local-package-name1 local-package-name2"
+# 		return
+# 	fi
 
-#     if [ $1 = "pkgs-local-remove" ]; then
-#         echo "local-package-name1 local-package-name2"
-#         return
-#     fi
+# 	if [ $1 = "pkgs-local-remove" ]; then
+# 		echo "local-package-name1 local-package-name2"
+# 		return
+# 	fi
 
-#     if [ $1 = "php-configure" ]; then
-#         docker-php-ext-configure
-#         return
-#     fi
+# 	if [ $1 = "php-configure" ]; then
+# 		docker-php-ext-configure
+# 		return
+# 	fi
 
-#     if [ $1 = "php-install" ]; then
-#         echo "php extension to install with docker-php-ext-install"
-#         return
-#     fi
+# 	if [ $1 = "php-install" ]; then
+# 		echo "php extension to install with docker-php-ext-install"
+# 		return
+# 	fi
 
-#     if [ $1 = "manual-install" ]; then
-#         manual installation
-#         return
-#     fi
+# 	if [ $1 = "manual-install" ]; then
+# 		manual installation
+# 		return
+# 	fi
 # }
 
 gd_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libsodium freetype libjpeg-turbo libpng"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libsodium freetype libjpeg-turbo libpng"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libsodium-dev freetype-dev libjpeg-turbo-dev libpng-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libsodium-dev freetype-dev libjpeg-turbo-dev libpng-dev"
+		return
+	fi
 
-    if [ $1 = "php-configure" ]; then
-        docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
-        return
-    fi
+	if [ $1 = "php-configure" ]; then
+		docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "gd"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "gd"
+		return
+	fi
 }
 
 intl_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libintl icu-libs"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libintl icu-libs"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "icu-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "icu-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "intl"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "intl"
+		return
+	fi
 }
 
 zip_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "zip libzip"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "zip libzip"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libzip-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libzip-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "zip"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "zip"
+		return
+	fi
 }
 
 soap_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libxml2"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libxml2"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libxml2-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libxml2-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "soap"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "soap"
+		return
+	fi
 }
 
 pdo_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "pdo"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "pdo"
+		return
+	fi
 }
 
 pdo_pgsql_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libpq"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libpq"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libpq-dev"
-        # postgresql-dev
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libpq-dev"
+		# postgresql-dev
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "pdo_pgsql"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "pdo_pgsql"
+		return
+	fi
 }
 
 pdo_mysql_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "pdo_mysql"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "pdo_mysql"
+		return
+	fi
 }
 
 mysqli_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "mysqli"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "mysqli"
+		return
+	fi
 }
 
 bcmath_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "bcmath"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "bcmath"
+		return
+	fi
 }
 
 opcache_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "opcache"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "opcache"
+		return
+	fi
 }
 
 igbinary_ext()
 {
-    if [ $1 = "manual-install" ]; then
-        pecl install igbinary
-        docker-php-ext-enable igbinary
-        return
-    fi
+	if [ $1 = "manual-install" ]; then
+		pecl install igbinary
+		docker-php-ext-enable igbinary
+		return
+	fi
 }
 
 memcached_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libmemcached-libs zlib libsasl"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libmemcached-libs zlib libsasl"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libmemcached-dev zlib-dev cyrus-sasl-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libmemcached-dev zlib-dev cyrus-sasl-dev"
+		return
+	fi
 
-    if [ $1 = "manual-install" ]; then
-        (
-            pecl install --nobuild memcached
-            cd "$(pecl config-get temp_dir)/memcached"
-            phpize
-            ./configure --enable-memcached-igbinary
-            make -j$(nproc)
-            make install
-        )
-        docker-php-ext-enable memcached
-        return
-    fi
+	if [ $1 = "manual-install" ]; then
+		(
+			pecl install --nobuild memcached
+			cd "$(pecl config-get temp_dir)/memcached"
+			phpize
+			./configure --enable-memcached-igbinary
+			make -j$(nproc)
+			make install
+		)
+		docker-php-ext-enable memcached
+		return
+	fi
 }
 
 redis_ext()
 {
-    if [ $1 = "manual-install" ]; then
-        (
-            pecl install --nobuild redis
-            cd "$(pecl config-get temp_dir)/redis"
-            phpize
-            ./configure --enable-redis-igbinary
-            make -j$(nproc)
-            make install
-        )
-        docker-php-ext-enable redis
-        return
-    fi
+	if [ $1 = "manual-install" ]; then
+		(
+			pecl install --nobuild redis
+			cd "$(pecl config-get temp_dir)/redis"
+			phpize
+			./configure --enable-redis-igbinary
+			make -j$(nproc)
+			make install
+		)
+		docker-php-ext-enable redis
+		return
+	fi
 }
 
 xdebug_ext()
 {
-    if [ $1 = "pkgs-dev" ]; then
-        echo "linux-headers"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "linux-headers"
+		return
+	fi
 
-    if [ $1 = "manual-install" ]; then
-        pecl install xdebug
-        docker-php-ext-enable xdebug
+	if [ $1 = "manual-install" ]; then
+		pecl install xdebug
+		docker-php-ext-enable xdebug
 
-        echo "xdebug.client_host=host.docker.internal" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
-        echo "xdebug.mode=develop,debug" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
-        return
-    fi
+		echo "xdebug.client_host=host.docker.internal" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
+		echo "xdebug.mode=develop,debug" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
+		return
+	fi
 }
 
 install_packages() {
-    if [ -z "$@" ]; then
-        echo "No local packages for installation"
-        return
-    fi
+	if [ -z "$@" ]; then
+		echo "No local packages for installation"
+		return
+	fi
 
-    local package_list="$@"
+	local package_list="$@"
 
-    # Install the list of packages
-    echo "Packages to verify are installed: ${package_list}"
-    eval "apk add --update --no-cache $package_list"
+	# Install the list of packages
+	echo "Packages to verify are installed: ${package_list}"
+	eval "apk add --update --no-cache $package_list"
 
-    # Get to latest versions of all packages
-    if [ "${UPGRADE_PACKAGES}" = "true" ]; then
-        apk upgrade -a
-    fi
+	# Get to latest versions of all packages
+	if [ "${UPGRADE_PACKAGES}" = "true" ]; then
+		apk upgrade -a
+	fi
 }
 
 install_dev_packages() {
-    local package_list="$@ \
-        $PHPIZE_DEPS"
+	local package_list="$@ \
+		$PHPIZE_DEPS"
 
-    # Install the list of packages
-    echo "Packages to verify are installed: ${package_list}"
-    eval "apk add --no-cache --update --virtual .dev_packages $package_list"
+	# Install the list of packages
+	echo "Packages to verify are installed: ${package_list}"
+	eval "apk add --no-cache --update --virtual .dev_packages $package_list"
 }
 
 install_local_packages() {
-    local list= "$(echo "$@" | sed -e "s,[[:space:]\n]\+, ,g" -e "s,^[[:space:]]\+\|[[:space:]]\+$,,g")"
+	local list= "$(echo "$@" | sed -e "s,[[:space:]\n]\+, ,g" -e "s,^[[:space:]]\+\|[[:space:]]\+$,,g")"
 
-    if [ -z "$list" ]; then
-        echo "No local packages for installation"
-        return
-    fi
+	if [ -z "$list" ]; then
+		echo "No local packages for installation"
+		return
+	fi
 
-    local package_list=$(echo "$list" | sed -e "s,\([^[:space:]]\+\),$LOCAL_PKG_DIR/\1,g")
+	local package_list=$(echo "$list" | sed -e "s,\([^[:space:]]\+\),$LOCAL_PKG_DIR/\1,g")
 
-    echo "Packages to verify are installed: ${package_list}"
+	echo "Packages to verify are installed: ${package_list}"
 
-    touch repo.list
-    eval "apk add --repositories-file=repo.list --allow-untrusted --no-network --no-cache $package_list"
+	touch repo.list
+	eval "apk add --repositories-file=repo.list --allow-untrusted --no-network --no-cache $package_list"
 }
 
 remove_dev_packages() {
-    apk del .dev_packages
+	apk del .dev_packages
 }
 
 remove_packages() {
-    local list=$(echo "$@" | sed -e "s,[[:space:]\n]\+, ,g" -e "s,^[[:space:]]\+\|[[:space:]]\+$,,g")
+	local list=$(echo "$@" | sed -e "s,[[:space:]\n]\+, ,g" -e "s,^[[:space:]]\+\|[[:space:]]\+$,,g")
 
-    if [ -z "$list" ]; then
-        return
-    fi
+	if [ -z "$list" ]; then
+		return
+	fi
 
-    apk del $list
+	apk del $list
 }
 
 # Actual package installation
@@ -325,13 +325,13 @@ remove_packages() {
 echo "Packages installation.."
 pkgs=""
 for func in $func_list; do
-    pkgs="${pkgs} $($func pkgs)"
+	pkgs="${pkgs} $($func pkgs)"
 done
 install_packages "$pkgs"
 
 pkgs=""
 for func in $func_list; do
-    pkgs="${pkgs} $($func pkgs-dev)"
+	pkgs="${pkgs} $($func pkgs-dev)"
 done
 
 install_dev_packages "$pkgs"
@@ -339,38 +339,38 @@ install_dev_packages "$pkgs"
 echo "Local packages installation.."
 local_pkgs=""
 for func in $func_list; do
-    local_pkgs="${local_pkgs} $($func pkgs-local)"
+	local_pkgs="${local_pkgs} $($func pkgs-local)"
 done
 
 install_local_packages "$local_pkgs"
 
 echo "Packages configuration.."
 for func in $func_list; do
-    echo "configuration: $(echo "$func" | sed -n "s/\([^[:space:]]\+\)/\1_ext/g")"
-    $func php-configure
+	echo "configuration: $(echo "$func" | sed -n "s/\([^[:space:]]\+\)/\1_ext/g")"
+	$func php-configure
 done
 
 install_list=""
 for func in $func_list; do
-    install_list="${install_list} $($func php-install)"
+	install_list="${install_list} $($func php-install)"
 done
 
 if [ -n "$install_list" ]; then
-    echo "PHP packages installation.."
+	echo "PHP packages installation.."
 
-    echo "docker-php-ext-install -j\"$(nproc)\" $install_list"
-    docker-php-ext-install -j"$(nproc)" $install_list
+	echo "docker-php-ext-install -j\"$(nproc)\" $install_list"
+	docker-php-ext-install -j"$(nproc)" $install_list
 fi
 
 for func in $func_list; do
-    echo "Manual installation of $(echo "$func" | sed -n 's/\(.\+\)_ext/\1/p')"
-    $func manual-install
+	echo "Manual installation of $(echo "$func" | sed -n 's/\(.\+\)_ext/\1/p')"
+	$func manual-install
 done
 
 echo "System cleanup.."
 pkgs=""
 for func in $func_list; do
-    pkgs="${pkgs} $($func pkgs-local-remove)"
+	pkgs="${pkgs} $($func pkgs-local-remove)"
 done
 
 remove_packages "$pkgs"
--- tools/bookworm.sh
+++ tools/bookworm.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-    exit 1
+	echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+	exit 1
 fi
 
 EXT_PACKAGES=${1:?}
@@ -23,312 +23,312 @@ func_list=$(echo $EXT_PACKAGES | sed -e 's/\([^[:space:]]\+\)/\1_ext/g')
 #####
 # example_ext()
 # {
-#     if [ $1 = "pkgs" ]; then
-#         echo "required packaged"
-#         return
-#     fi
+# 	if [ $1 = "pkgs" ]; then
+# 		echo "required packaged"
+# 		return
+# 	fi
 
-#     if [ $1 = "pkgs-dev" ]; then
-#         echo "packages to build"
-#         return
-#     fi
+# 	if [ $1 = "pkgs-dev" ]; then
+# 		echo "packages to build"
+# 		return
+# 	fi
 
-#     if [ $1 = "pkgs-local" ]; then
-#         echo "local-package-file-name1 local-package-file-name2"
-#         return
-#     fi
+# 	if [ $1 = "pkgs-local" ]; then
+# 		echo "local-package-file-name1 local-package-file-name2"
+# 		return
+# 	fi
 
-#     if [ $1 = "pkgs-local-remove" ]; then
-#         echo "local-package-name1 local-package-name2"
-#         return
-#     fi
+# 	if [ $1 = "pkgs-local-remove" ]; then
+# 		echo "local-package-name1 local-package-name2"
+# 		return
+# 	fi
 
-#     if [ $1 = "php-configure" ]; then
-#         docker-php-ext-configure
-#         return
-#     fi
+# 	if [ $1 = "php-configure" ]; then
+# 		docker-php-ext-configure
+# 		return
+# 	fi
 
-#     if [ $1 = "php-install" ]; then
-#         echo "php extension to install with docker-php-ext-install"
-#         return
-#     fi
+# 	if [ $1 = "php-install" ]; then
+# 		echo "php extension to install with docker-php-ext-install"
+# 		return
+# 	fi
 
-#     if [ $1 = "manual-install" ]; then
-#         manual installation
-#         return
-#     fi
+# 	if [ $1 = "manual-install" ]; then
+# 		manual installation
+# 		return
+# 	fi
 # }
 
 gd_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libsodium23 libfreetype6 libjpeg62-turbo libpng16-16"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libsodium23 libfreetype6 libjpeg62-turbo libpng16-16"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo " libsodium-dev libfreetype-dev libjpeg62-turbo-dev libpng-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo " libsodium-dev libfreetype-dev libjpeg62-turbo-dev libpng-dev"
+		return
+	fi
 
-    if [ $1 = "php-configure" ]; then
-        docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
-        return
-    fi
+	if [ $1 = "php-configure" ]; then
+		docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "gd"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "gd"
+		return
+	fi
 }
 
 intl_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "gettext libicu72"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "gettext libicu72"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libicu-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libicu-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "intl"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "intl"
+		return
+	fi
 }
 
 zip_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "zip libzip4"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "zip libzip4"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libzip-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libzip-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "zip"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "zip"
+		return
+	fi
 }
 
 soap_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libxml2"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libxml2"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libxml2-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libxml2-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "soap"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "soap"
+		return
+	fi
 }
 
 pdo_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "pdo"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "pdo"
+		return
+	fi
 }
 
 pdo_pgsql_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libpq5"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libpq5"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libpq-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libpq-dev"
+		return
+	fi
 
-    if [ $1 = "php-install" ]; then
-        echo "pdo_pgsql"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "pdo_pgsql"
+		return
+	fi
 }
 
 pdo_mysql_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "pdo_mysql"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "pdo_mysql"
+		return
+	fi
 }
 
 mysqli_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "mysqli"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "mysqli"
+		return
+	fi
 }
 
 bcmath_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "bcmath"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "bcmath"
+		return
+	fi
 }
 
 opcache_ext()
 {
-    if [ $1 = "php-install" ]; then
-        echo "opcache"
-        return
-    fi
+	if [ $1 = "php-install" ]; then
+		echo "opcache"
+		return
+	fi
 }
 
 igbinary_ext()
 {
-    if [ $1 = "manual-install" ]; then
-        pecl install igbinary
-        docker-php-ext-enable igbinary
-        return
-    fi
+	if [ $1 = "manual-install" ]; then
+		pecl install igbinary
+		docker-php-ext-enable igbinary
+		return
+	fi
 }
 
 memcached_ext()
 {
-    if [ $1 = "pkgs" ]; then
-        echo "libmemcachedutil2 zlib1g libsasl2-2"
-        return
-    fi
+	if [ $1 = "pkgs" ]; then
+		echo "libmemcachedutil2 zlib1g libsasl2-2"
+		return
+	fi
 
-    if [ $1 = "pkgs-dev" ]; then
-        echo "libmemcached-dev zlib1g-dev libsasl2-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		echo "libmemcached-dev zlib1g-dev libsasl2-dev"
+		return
+	fi
 
-    if [ $1 = "pkgs-local" ]; then
-        # https://github.com/php-memcached-dev/php-memcached/issues/140
-        echo "\
-        libssl1.1_1.1.1n-0+deb11u4_amd64.deb \
-        libssl-dev_1.1.1n-0+deb10u3_amd64.deb"
-        return
-    fi
+	if [ $1 = "pkgs-local" ]; then
+		# https://github.com/php-memcached-dev/php-memcached/issues/140
+		echo "\
+		libssl1.1_1.1.1n-0+deb11u4_amd64.deb \
+		libssl-dev_1.1.1n-0+deb10u3_amd64.deb"
+		return
+	fi
 
-    if [ $1 = "pkgs-local-remove" ]; then
-        echo "libssl-dev"
-        return
-    fi
+	if [ $1 = "pkgs-local-remove" ]; then
+		echo "libssl-dev"
+		return
+	fi
 
-    if [ $1 = "manual-install" ]; then
-        (
-            pecl install --nobuild memcached
-            cd "$(pecl config-get temp_dir)/memcached"
-            phpize
-            ./configure --enable-memcached-igbinary
-            make -j$(nproc)
-            make install
-        )
-        docker-php-ext-enable memcached
-        return
-    fi
+	if [ $1 = "manual-install" ]; then
+		(
+			pecl install --nobuild memcached
+			cd "$(pecl config-get temp_dir)/memcached"
+			phpize
+			./configure --enable-memcached-igbinary
+			make -j$(nproc)
+			make install
+		)
+		docker-php-ext-enable memcached
+		return
+	fi
 }
 
 redis_ext()
 {
-    if [ $1 = "manual-install" ]; then
-        (
-            pecl install --nobuild redis
-            cd "$(pecl config-get temp_dir)/redis"
-            phpize
-            ./configure --enable-redis-igbinary
-            make -j$(nproc)
-            make install
-        )
-        docker-php-ext-enable redis
-        return
-    fi
+	if [ $1 = "manual-install" ]; then
+		(
+			pecl install --nobuild redis
+			cd "$(pecl config-get temp_dir)/redis"
+			phpize
+			./configure --enable-redis-igbinary
+			make -j$(nproc)
+			make install
+		)
+		docker-php-ext-enable redis
+		return
+	fi
 }
 
 xdebug_ext()
 {
-    if [ $1 = "pkgs-dev" ]; then
-        # headers are included into debian image already
-        # echo "linux-libc-dev"
-        return
-    fi
+	if [ $1 = "pkgs-dev" ]; then
+		# headers are included into debian image already
+		# echo "linux-libc-dev"
+		return
+	fi
 
-    if [ $1 = "manual-install" ]; then
-        pecl install xdebug
-        docker-php-ext-enable xdebug
+	if [ $1 = "manual-install" ]; then
+		pecl install xdebug
+		docker-php-ext-enable xdebug
 
-        echo "xdebug.client_host=host.docker.internal" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
-        echo "xdebug.mode=develop,debug" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
-        return
-    fi
+		echo "xdebug.client_host=host.docker.internal" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
+		echo "xdebug.mode=develop,debug" >> ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
+		return
+	fi
 }
 
 install_packages() {
-    # Ensure apt is in non-interactive to avoid prompts
-    export DEBIAN_FRONTEND=noninteractive
+	# Ensure apt is in non-interactive to avoid prompts
+	export DEBIAN_FRONTEND=noninteractive
 
-    local package_list="$@ \
-        $PHPIZE_DEPS"
+	local package_list="$@ \
+		$PHPIZE_DEPS"
 
-    # Install the list of packages
-    echo "Packages to verify are installed: ${package_list}"
-    rm -rf /var/lib/apt/lists/*
-    apt-get update -y
-    apt-get -y install --no-install-recommends ${package_list}
+	# Install the list of packages
+	echo "Packages to verify are installed: ${package_list}"
+	rm -rf /var/lib/apt/lists/*
+	apt-get update -y
+	apt-get -y install --no-install-recommends ${package_list}
 
-    # Get to latest versions of all packages
-    if [ "${UPGRADE_PACKAGES}" = "true" ]; then
-        apt-get -y upgrade --no-install-recommends
-        apt-get autoremove -y
-    fi
+	# Get to latest versions of all packages
+	if [ "${UPGRADE_PACKAGES}" = "true" ]; then
+		apt-get -y upgrade --no-install-recommends
+		apt-get autoremove -y
+	fi
 }
 
 install_local_packages() {
-    local list=$(echo "$@" | sed -e "s,[[:space:]\n]\+, ,g" -e "s,^[[:space:]]\+\|[[:space:]]\+$,,g")
+	local list=$(echo "$@" | sed -e "s,[[:space:]\n]\+, ,g" -e "s,^[[:space:]]\+\|[[:space:]]\+$,,g")
 
-    if [ -z "$list" ]; then
-        echo "No local packages for installation"
-        return
-    fi
+	if [ -z "$list" ]; then
+		echo "No local packages for installation"
+		return
+	fi
 
-    local package_list=$(echo "$list" | sed -e "s,\([^[:space:]]\+\),$LOCAL_PKG_DIR/\1,g")
+	local package_list=$(echo "$list" | sed -e "s,\([^[:space:]]\+\),$LOCAL_PKG_DIR/\1,g")
 
-    echo "Packages to verify are installed: ${package_list}"
+	echo "Packages to verify are installed: ${package_list}"
 
-    for package in $package_list; do
-        eval "dpkg -i $package"
-    done
+	for package in $package_list; do
+		eval "dpkg -i $package"
+	done
 
-    apt-get install -f
+	apt-get install -f
 }
 
 remove_packages() {
-     # Ensure apt is in non-interactive to avoid prompts
-    export DEBIAN_FRONTEND=noninteractive
+	 # Ensure apt is in non-interactive to avoid prompts
+	export DEBIAN_FRONTEND=noninteractive
 
-    local package_list="$@"
+	local package_list="$@"
 
-    echo "Packages to verify are removed: ${package_list}"
-    # Remove the list of packages
-    apt-get -y remove --no-install-recommends ${package_list}
-    apt-get autoremove -y
+	echo "Packages to verify are removed: ${package_list}"
+	# Remove the list of packages
+	apt-get -y remove --no-install-recommends ${package_list}
+	apt-get autoremove -y
 
-    # Clean up
-    apt-get -y clean
-    rm -rf /var/lib/apt/lists/*
+	# Clean up
+	apt-get -y clean
+	rm -rf /var/lib/apt/lists/*
 }
 
 # Actual package installation
@@ -336,8 +336,8 @@ remove_packages() {
 echo "Packages installation.."
 pkgs=""
 for func in $func_list; do
-    pkgs="${pkgs} $($func pkgs)"
-    pkgs="${pkgs} $($func pkgs-dev)"
+	pkgs="${pkgs} $($func pkgs)"
+	pkgs="${pkgs} $($func pkgs-dev)"
 done
 
 install_packages "$pkgs"
@@ -345,38 +345,38 @@ install_packages "$pkgs"
 echo "Local packages installation.."
 local_pkgs=""
 for func in $func_list; do
-    local_pkgs="${local_pkgs} $($func pkgs-local)"
+	local_pkgs="${local_pkgs} $($func pkgs-local)"
 done
 
 install_local_packages "$local_pkgs"
 
 echo "Packages configuration.."
 for func in $func_list; do
-    echo "configuration: $(echo "$func" | sed -n "s/\([^[:space:]]\+\)/\1_ext/g")"
-    $func php-configure
+	echo "configuration: $(echo "$func" | sed -n "s/\([^[:space:]]\+\)/\1_ext/g")"
+	$func php-configure
 done
 
 install_list=""
 for func in $func_list; do
-    install_list="${install_list} $($func php-install)"
+	install_list="${install_list} $($func php-install)"
 done
 
 if [ -n "$install_list" ]; then
-    echo "PHP packages installation.."
+	echo "PHP packages installation.."
 
-    echo "docker-php-ext-install -j\"$(nproc)\" $install_list"
-    docker-php-ext-install -j"$(nproc)" $install_list
+	echo "docker-php-ext-install -j\"$(nproc)\" $install_list"
+	docker-php-ext-install -j"$(nproc)" $install_list
 fi
 
 for func in $func_list; do
-    echo "Manual installation of $(echo "$func" | sed -n 's/\(.\+\)_ext/\1/p')"
-    $func manual-install
+	echo "Manual installation of $(echo "$func" | sed -n 's/\(.\+\)_ext/\1/p')"
+	$func manual-install
 done
 
 pkgs=""
 for func in $func_list; do
-    pkgs="${pkgs} $($func pkgs-dev)"
-    pkgs="${pkgs} $($func pkgs-local-remove)"
+	pkgs="${pkgs} $($func pkgs-dev)"
+	pkgs="${pkgs} $($func pkgs-local-remove)"
 done
 
 echo "System cleanup.."

c783c19dc258dd553197fab422205f3c64c9db82
bagermen <bagermen@yandex.ru>
2024-06-23 19:28:59 +0300

    ci: :alembic: create custom action

---
+++ .github/actions/extract-parameters/dist/index.js
@@ -0,0 +1,154 @@
+/******/ var __webpack_modules__ = ({
+
+/***/ 168:
+/***/ ((__webpack_module__, __unused_webpack___webpack_exports__, __nccwpck_require__) => {
+
+__nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+const core = require("@actions/core");
+const glob = require("@actions/glob");
+
+try {
+	const phpVersion = core.getInput("php-version", {required: true});
+	const phpType = core.getInput("php-type", {required: false});
+
+	const dockerFiles = "Dockerfile.*";
+	const globber = await glob.create(dockerFiles, {followSymbolicLinks: false});
+	const files = await globber.glob();
+
+	core.setOutput("image-context", JSON.stringify(createOutput(phpVersion, files, phpType)));
+} catch (error) {
+	core.setFailed(error.message);
+}
+
+function getImageName(phpVersion, osName, phpType) {
+	return `${phpVersion}${phpType ? `-${phpType}-` : ''}${osName}`;
+}
+
+function createOutput(phpVersion, files, phpType) {
+	const osList = files.map(file => file.replace(/.*Dockerfile\./, ""));
+
+	return osList.map((osName, idx) => ({
+		image: getImageName(phpVersion, osName, phpType),
+		dockerFile: files[idx]
+	}));
+}
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } }, 1);
+
+/***/ })
+
+/******/ });
+/************************************************************************/
+/******/ // The module cache
+/******/ var __webpack_module_cache__ = {};
+/******/ 
+/******/ // The require function
+/******/ function __nccwpck_require__(moduleId) {
+/******/ 	// Check if module is in cache
+/******/ 	var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 	if (cachedModule !== undefined) {
+/******/ 		return cachedModule.exports;
+/******/ 	}
+/******/ 	// Create a new module (and put it into the cache)
+/******/ 	var module = __webpack_module_cache__[moduleId] = {
+/******/ 		// no module.id needed
+/******/ 		// no module.loaded needed
+/******/ 		exports: {}
+/******/ 	};
+/******/ 
+/******/ 	// Execute the module function
+/******/ 	var threw = true;
+/******/ 	try {
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 		threw = false;
+/******/ 	} finally {
+/******/ 		if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 	}
+/******/ 
+/******/ 	// Return the exports of the module
+/******/ 	return module.exports;
+/******/ }
+/******/ 
+/************************************************************************/
+/******/ /* webpack/runtime/async module */
+/******/ (() => {
+/******/ 	var webpackQueues = typeof Symbol === "function" ? Symbol("webpack queues") : "__webpack_queues__";
+/******/ 	var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
+/******/ 	var webpackError = typeof Symbol === "function" ? Symbol("webpack error") : "__webpack_error__";
+/******/ 	var resolveQueue = (queue) => {
+/******/ 		if(queue && !queue.d) {
+/******/ 			queue.d = 1;
+/******/ 			queue.forEach((fn) => (fn.r--));
+/******/ 			queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
+/******/ 		}
+/******/ 	}
+/******/ 	var wrapDeps = (deps) => (deps.map((dep) => {
+/******/ 		if(dep !== null && typeof dep === "object") {
+/******/ 			if(dep[webpackQueues]) return dep;
+/******/ 			if(dep.then) {
+/******/ 				var queue = [];
+/******/ 				queue.d = 0;
+/******/ 				dep.then((r) => {
+/******/ 					obj[webpackExports] = r;
+/******/ 					resolveQueue(queue);
+/******/ 				}, (e) => {
+/******/ 					obj[webpackError] = e;
+/******/ 					resolveQueue(queue);
+/******/ 				});
+/******/ 				var obj = {};
+/******/ 				obj[webpackQueues] = (fn) => (fn(queue));
+/******/ 				return obj;
+/******/ 			}
+/******/ 		}
+/******/ 		var ret = {};
+/******/ 		ret[webpackQueues] = x => {};
+/******/ 		ret[webpackExports] = dep;
+/******/ 		return ret;
+/******/ 	}));
+/******/ 	__nccwpck_require__.a = (module, body, hasAwait) => {
+/******/ 		var queue;
+/******/ 		hasAwait && ((queue = []).d = 1);
+/******/ 		var depQueues = new Set();
+/******/ 		var exports = module.exports;
+/******/ 		var currentDeps;
+/******/ 		var outerResolve;
+/******/ 		var reject;
+/******/ 		var promise = new Promise((resolve, rej) => {
+/******/ 			reject = rej;
+/******/ 			outerResolve = resolve;
+/******/ 		});
+/******/ 		promise[webpackExports] = exports;
+/******/ 		promise[webpackQueues] = (fn) => (queue && fn(queue), depQueues.forEach(fn), promise["catch"](x => {}));
+/******/ 		module.exports = promise;
+/******/ 		body((deps) => {
+/******/ 			currentDeps = wrapDeps(deps);
+/******/ 			var fn;
+/******/ 			var getResult = () => (currentDeps.map((d) => {
+/******/ 				if(d[webpackError]) throw d[webpackError];
+/******/ 				return d[webpackExports];
+/******/ 			}))
+/******/ 			var promise = new Promise((resolve) => {
+/******/ 				fn = () => (resolve(getResult));
+/******/ 				fn.r = 0;
+/******/ 				var fnQueue = (q) => (q !== queue && !depQueues.has(q) && (depQueues.add(q), q && !q.d && (fn.r++, q.push(fn))));
+/******/ 				currentDeps.map((dep) => (dep[webpackQueues](fnQueue)));
+/******/ 			});
+/******/ 			return fn.r ? promise : getResult();
+/******/ 		}, (err) => ((err ? reject(promise[webpackError] = err) : outerResolve(exports)), resolveQueue(queue)));
+/******/ 		queue && (queue.d = 0);
+/******/ 	};
+/******/ })();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+/******/ 
+/******/ // startup
+/******/ // Load entry module and return exports
+/******/ // This entry module used 'module' so it can't be inlined
+/******/ var __webpack_exports__ = __nccwpck_require__(168);
+/******/ __webpack_exports__ = await __webpack_exports__;
+/******/ 
---
+++ .github/actions/extract-parameters/index.js
@@ -0,0 +1,28 @@
+const core = require("@actions/core");
+const glob = require("@actions/glob");
+
+try {
+	const phpVersion = core.getInput("php-version", {required: true});
+	const phpType = core.getInput("php-type", {required: false});
+
+	const dockerFiles = "Dockerfile.*";
+	const globber = await glob.create(dockerFiles, {followSymbolicLinks: false});
+	const files = await globber.glob();
+
+	core.setOutput("image-context", JSON.stringify(createOutput(phpVersion, files, phpType)));
+} catch (error) {
+	core.setFailed(error.message);
+}
+
+function getImageName(phpVersion, osName, phpType) {
+	return `${phpVersion}${phpType ? `-${phpType}-` : ''}${osName}`;
+}
+
+function createOutput(phpVersion, files, phpType) {
+	const osList = files.map(file => file.replace(/.*Dockerfile\./, ""));
+
+	return osList.map((osName, idx) => ({
+		image: getImageName(phpVersion, osName, phpType),
+		dockerFile: files[idx]
+	}));
+}

b4ad154c988dfc7f36509314fc3a4391c9dae245
bagermen <bagermen@yandex.ru>
2024-06-23 20:29:55 +0300

    fix: :ambulance: try to fix action

---
+++ .github/actions/extract-parameters/index.ts
@@ -0,0 +1,42 @@
+import * as core from "@actions/core"
+import * as glob from "@actions/glob"
+
+async function Run() {
+	try {
+		const phpVersion = core.getInput("php-version", {required: true});
+		const phpType = core.getInput("php-type", {required: false});
+
+		const dockerFiles = "Dockerfile.*";
+		const globber = await glob.create(dockerFiles, {followSymbolicLinks: false});
+		const files = await globber.glob();
+
+		core.setOutput("image-context", JSON.stringify(createOutput(phpVersion, files, phpType)));
+	} catch (error: unknown) {
+		if (isError(error)) {
+			core.setFailed(error.message);
+		}
+	}
+}
+
+Run();
+
+function getImageName(phpVersion:string, osName:string, phpType:string) {
+	return `${phpVersion}${phpType ? `-${phpType}-` : ''}${osName}`;
+}
+
+function createOutput(phpVersion:string, files:string[], phpType:string) {
+	const osList = files.map(file => file.replace(/.*Dockerfile\./, ""));
+
+	return osList.map((osName, idx) => ({
+		image: getImageName(phpVersion, osName, phpType),
+		dockerFile: files[idx]
+	}));
+}
+
+function isError(error: unknown): error is Error {
+	if (error && typeof error === "object" && "message" in error) {
+		return true;
+	}
+
+	return false;
+}
